dff320f18665f670e39e42115ae22fe9c6c61893
Jannik <jannik@ramrath.me>
2023-10-25 14:25:58 +0200

    add abbreviations for color-functions
    Signed-off-by: Jannik <jannik@ramrath.me>

--- textColor/__init__.py
+++ textColor/__init__.py
@@ -4,17 +4,20 @@ def green(text, whiteText="", space=True):
     else:
         return "\033[2;32;49m" + str(text) + "\033[0;37;49m" + str(whiteText)
 
+
 def red(text, whiteText="", space=True):
     if space:
         return "\033[1;31;49m" + str(text) + " \033[0;37;49m" + str(whiteText)
     else:
         return "\033[1;31;49m" + str(text) + "\033[0;37;49m" + str(whiteText)
 
+
 def blue(text, whiteText="", space=True):
     if space:
         return "\033[1;34;49m" + str(text) + " \033[0;37;49m" + str(whiteText)
     else:
         return "\033[1;34;49m" + str(text) + "\033[0;37;49m" + str(whiteText)
+
 
 def yellow(text, whiteText="", space=True):
     if space:
@@ -23,14 +26,33 @@ def yellow(text, whiteText="", space=True):
         return "\033[1;33;49m" + str(text) + "\033[0;37;49m" + str(whiteText)
 
 
+def g(*args, **kwargs):
+    return green(*args, **kwargs)
+
+
+def r(*args, **kwargs):
+    return green(*args, **kwargs)
+
+
+def b(*args, **kwargs):
+    return green(*args, **kwargs)
+
+
+def y(*args, **kwargs):
+    return green(*args, **kwargs)
+
+
 def input(text):
     return "\033[1;33;49m" + "[?]" + " \033[0;37;49m" + str(text)
+
 
 def info(text):
     return "\033[1;34;49m" + "[-]" + " \033[0;37;49m" + str(text)
 
+
 def error(text):
     return "\033[1;31;49m" + "[!]" + " \033[0;37;49m" + str(text)
+
 
 def output(text):
     return "\033[1;32;49m" + "[>]" + " \033[0;37;49m" + str(text)
